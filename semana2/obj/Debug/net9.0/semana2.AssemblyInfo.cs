73b2e14243b114c107bbd60f21e5668d77ab015f
Yessenia24 <ye.rodriguez-uea.edu.ec>
2025-06-14 16:35:01 -0500

    semana2
    semana2

--- semana2/obj/Debug/net9.0/semana2.AssemblyInfo.cs
+++ semana2/obj/Debug/net9.0/semana2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("semana2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77e0a3b4874a6661d3474e010117f722c8a15e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2393fc6f0b8141d5a5d87cbfbe55e4f66e22684")]
 [assembly: System.Reflection.AssemblyProductAttribute("semana2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("semana2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
