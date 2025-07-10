009bd4cca9e9a4d0cf9e30137af20c50502e4c33
Yessenia24 <ye.rodriguez-uea.edu.ec>
2025-07-10 18:09:10 -0500

    SEMANA7

---
+++ semana7.1/resolución.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TorresDeHanoi
+{
+    static Stack<int> torreA = new Stack<int>();
+    static Stack<int> torreB = new Stack<int>();
+    static Stack<int> torreC = new Stack<int>();
+
+    static void MoverDiscos(int n, Stack<int> origen, Stack<int> destino, Stack<int> auxiliar, string nombreOrigen, string nombreDestino, string nombreAuxiliar)
+    {
+        if (n == 1)
+        {
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine($"Mover disco {disco} de {nombreOrigen} a {nombreDestino}");
+            MostrarTorres();
+            return;
+        }
+
+        MoverDiscos(n - 1, origen, auxiliar, destino, nombreOrigen, nombreAuxiliar, nombreDestino);
+        MoverDiscos(1, origen, destino, auxiliar, nombreOrigen, nombreDestino, nombreAuxiliar);
+        MoverDiscos(n - 1, auxiliar, destino, origen, nombreAuxiliar, nombreDestino, nombreOrigen);
+    }
+
+    static void MostrarTorres()
+    {
+        Console.WriteLine("Torre A: " + string.Join(", ", torreA));
+        Console.WriteLine("Torre B: " + string.Join(", ", torreB));
+        Console.WriteLine("Torre C: " + string.Join(", ", torreC));
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        int numDiscos = 3;
+
+        // Inicializar torre A con discos (mayor en el fondo)
+        for (int i = numDiscos; i >= 1; i--)
+        {
+            torreA.Push(i);
+        }
+
+        Console.WriteLine("Estado inicial:");
+        MostrarTorres();
+
+        MoverDiscos(numDiscos, torreA, torreC, torreB, "A", "C", "B");
+
+        Console.WriteLine("Estado final:");
+        MostrarTorres();
+    }
+}
--- semana7/obj/Debug/net9.0/semana7.AssemblyInfo.cs
+++ semana7/obj/Debug/net9.0/semana7.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("semana2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("semana7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3fc6d1ea110c7256f335009debc16e146d6524b")]
-[assembly: System.Reflection.AssemblyProductAttribute("semana2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("semana2")]
+[assembly: System.Reflection.AssemblyProductAttribute("semana7")]
+[assembly: System.Reflection.AssemblyTitleAttribute("semana7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
